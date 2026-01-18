c10789067d6029fda3bc55ac14bb20c34f046510
0-Zt <salinas.ssd@gmail.com>
2026-01-18 13:53:31 -0300

    fix

--- src/app/services/api.config.ts
+++ src/app/services/api.config.ts
@@ -1,3 +1,5 @@
-export const API_BASE_URL = 'http://localhost:3000';
+
+export const API_BASE_URL = 'https://finanzapp-backend-77hj.onrender.com'; 
 
 //'https://finanzapp-backend-77hj.onrender.com'
+// http://localhost:3000
